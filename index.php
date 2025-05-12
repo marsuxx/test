428fa491d7793eb8dec8cc376509e3502467ba68
marsuxx <ilhantuani3@gmail.com>
2025-05-12 10:33:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+        <div class="message"><?= htmlspecialchars($message) ?></div>
+    <?php endif; ?>
+    <h2>Nos voyages disponibles</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Destination</th>
+                <th>Date de départ</th>
+                <th>Durée (jours)</th>
+                <th>Prix (€)</th>
+                <th>Places disponibles</th>
+                <th>Réserver</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($voyages as $index => $v): ?>
+            <tr>
+                <td><?= htmlspecialchars($v->getDestination()) ?></td>
+                <td><?= htmlspecialchars($v->getDateDepart()) ?></td>
+                <td><?= htmlspecialchars($v->getDuree()) ?></td>
+                <td><?= number_format($v->getPrix(), 2, ',', ' ') ?></td>
+                <td><?= htmlspecialchars($v->getPlacesDisponibles()) ?></td>
+                <td>
+                    <?php if ($v->getPlacesDisponibles() > 0): ?>
+                    <form method="post" style="margin:0;">
+                        <input type="hidden" name="voyage_index" value="<?= $index ?>" />
+                        <label for="places_<?= $index ?>">Places:</label>
+                        <input type="number" id="places_<?= $index ?>" name="places" min="1" max="<?= $v->getPlacesDisponibles() ?>" value="1" required />
+                        <input type="submit" name="reserver" value="Réserver" />
+                    </form>
+                    <?php else: ?>
+                        Complet
+                    <?php endif; ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
